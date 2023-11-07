318551c9f0b298ac66073e64691b10e00d4651ac
oriolbernal <obernalpo@gmail.com>
2023-11-07 15:40:55 +0100

    First commit

---
+++ src/test/java/com/obernal/portal_monitoratge/PortalMonitoratgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obernal.portal_monitoratge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalMonitoratgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
