6515f67a9c84772aba7ffb7c06870d50e34133dd
oriolbernal <obernalpo@gmail.com>
2023-11-11 01:20:46 +0100

    ignore expired certificates test

---
+++ src/test/java/com/obernal/portal_monitoratge/model/monitor/impl/clients/IgnoreCertificateExpirationTrustManagerTest.java
@@ -0,0 +1,39 @@
+package com.obernal.portal_monitoratge.model.monitor.impl.clients;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
+import java.util.Date;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class IgnoreCertificateExpirationTrustManagerTest {
+
+    private IgnoreCertificateExpirationTrustManager trustManager;
+
+    @BeforeEach
+    public void setup() {
+        X509TrustManager innerTrustManager = mock(X509TrustManager.class);
+        trustManager = new IgnoreCertificateExpirationTrustManager(innerTrustManager);
+    }
+
+    @Test
+    public void checkServerTrusted_WithExpiredCertificate_ShouldNotThrowException() {
+        X509Certificate[] chain = new X509Certificate[1];
+        X509Certificate expiredCertificate = createExpiredCertificate();
+        chain[0] = expiredCertificate;
+        assertDoesNotThrow(() -> trustManager.checkServerTrusted(chain, "RSA"));
+    }
+
+    private X509Certificate createExpiredCertificate() {
+        X509Certificate certificate = mock(X509Certificate.class);
+        when(certificate.getNotAfter()).thenReturn(new Date(System.currentTimeMillis() - 1000));
+        return certificate;
+    }
+
+}
+
