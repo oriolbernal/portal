47473a35270d96ed8c5c0499bf0a80785899c0a5
oriolbernal <obernalpo@gmail.com>
2023-11-11 16:24:51 +0100

    add ssl test

--- src/test/java/com/obernal/portal_monitoratge/model/monitor/impl/SslMonitorTest.java
+++ src/test/java/com/obernal/portal_monitoratge/model/monitor/impl/SslMonitorTest.java
@@ -10,19 +10,198 @@ import static org.junit.jupiter.api.Assertions.*;
 class SslMonitorTest {
 
     @Test
-    void run() {
+    void sslCertificate_inLessThan_1ms() {
         SslMonitor monitor = new SslMonitor(
                 "id",
                 "https://google.es",
-                50,
-                HttpClient.Version.HTTP_1_1,
+                1,
+                HttpClient.Version.HTTP_2,
                 HttpClient.Redirect.NEVER,
                 new String[]{"TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"},
                 0
         );
         Execution execution = monitor.run();
-        assertNotNull(execution);
         assertFalse(execution.isError());
         assertFalse(execution.isAlert());
     }
+
+    @Test
+    void error_if_http() {
+        SslMonitor monitor = new SslMonitor(
+                "id",
+                "http://github.com",
+                1,
+                HttpClient.Version.HTTP_2,
+                HttpClient.Redirect.NEVER,
+                new String[]{"TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"},
+                0
+        );
+        Execution execution = monitor.run();
+        assertEquals("SSL certificate not found", execution.getErrorMessage());
+    }
+
+    @Test
+    void error_if_timeout() {
+        SslMonitor monitor = new SslMonitor(
+                "id",
+                "https://10.255.255.1/", // non-routable ip adress
+                1,
+                HttpClient.Version.HTTP_2,
+                HttpClient.Redirect.NEVER,
+                new String[]{"TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"},
+                0
+        );
+        Execution execution = monitor.run();
+        assertEquals("Connection Timeout: HTTP connect timed out", execution.getErrorMessage());
+    }
+
+    @Test
+    void error_if_tlsProtocol_notConfigured() {
+        SslMonitor monitor = new SslMonitor(
+                "id",
+                "https://github.com/",
+                1,
+                HttpClient.Version.HTTP_2,
+                HttpClient.Redirect.NEVER,
+                new String[]{},
+                0
+        );
+        Execution execution = monitor.run();
+        assertEquals("SSL Connection problem: No appropriate protocol (protocol is disabled or cipher suites are inappropriate)", execution.getErrorMessage());
+    }
+
+    @Test
+    void error_if_tlsProtocol_nonExistent() {
+        SslMonitor monitor = new SslMonitor(
+                "id",
+                "https://github.com/",
+                1,
+                HttpClient.Version.HTTP_2,
+                HttpClient.Redirect.NEVER,
+                new String[]{"A"},
+                0
+        );
+        Execution execution = monitor.run();
+        assertEquals("Nonexistent SSLProtocol: Unsupported protocol: A", execution.getErrorMessage());
+    }
+
+    @Test
+    void error_if_certificate_handshake() {
+        SslMonitor monitor = new SslMonitor(
+                "id",
+                "https://ssc.catcert.cat:8090/",
+                1,
+                HttpClient.Version.HTTP_2,
+                HttpClient.Redirect.NEVER,
+                new String[]{"TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"},
+                0
+        );
+        Execution execution = monitor.run();
+        assertEquals("SSL Connection problem: Remote host terminated the handshake", execution.getErrorMessage());
+    }
+
+    @Test
+    void error_if_http2() {
+        SslMonitor monitor = new SslMonitor(
+                "id",
+                "https://hestiacertif.aoc.cat",
+                1,
+                HttpClient.Version.HTTP_2,
+                HttpClient.Redirect.NEVER,
+                new String[]{"TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"},
+                0
+        );
+        Execution execution = monitor.run();
+        assertEquals("I/O error occurred when sending or receiving: Received RST_STREAM: Use HTTP/1.1 for request", execution.getErrorMessage());
+    }
+
+    @Test
+    void error_if_host_doesNotMatch_certificate() {
+        SslMonitor monitor = new SslMonitor(
+                "id",
+                "https://wrong.host.badssl.com/",
+                1,
+                HttpClient.Version.HTTP_2,
+                HttpClient.Redirect.NEVER,
+                new String[]{"TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"},
+                0
+        );
+        Execution execution = monitor.run();
+        assertEquals("SSL Connection problem: No subject alternative DNS name matching wrong.host.badssl.com found.", execution.getErrorMessage());
+    }
+
+    @Test
+    void error_if_host_unresolved() {
+        SslMonitor monitor = new SslMonitor(
+                "id",
+                "https://serveis3-dev.nt.aoc.cat",
+                1,
+                HttpClient.Version.HTTP_2,
+                HttpClient.Redirect.NEVER,
+                new String[]{"TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"},
+                0
+        );
+        Execution execution = monitor.run();
+        assertEquals("ConnectException (Unresolved host?): null", execution.getErrorMessage());
+    }
+
+    @Test
+    void error_if_certificate_untrusted_NotInCacerts() {
+        SslMonitor monitor = new SslMonitor(
+                "id",
+                "https://self-signed.badssl.com/",
+                1,
+                HttpClient.Version.HTTP_2,
+                HttpClient.Redirect.NEVER,
+                new String[]{"TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"},
+                0
+        );
+        Execution execution = monitor.run();
+        assertEquals("SSL Connection problem: PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException: unable to find valid certification path to requested target", execution.getErrorMessage());
+    }
+
+    @Test
+    void connect_with_clientAuth() {
+        SslMonitor monitor = new SslMonitor(
+                "id",
+                "https://etramrouter-pre.aoc.cat/etramrouter/Sincron#SSL",
+                1,
+                HttpClient.Version.HTTP_2,
+                HttpClient.Redirect.NEVER,
+                new String[]{"TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"},
+                0
+        );
+        assertDoesNotThrow(monitor::run);
+    }
+
+    @Test
+    void error_if_certificate_revoked() {
+        SslMonitor monitor = new SslMonitor(
+                "id",
+                "https://revoked.badssl.com/",
+                1,
+                HttpClient.Version.HTTP_2,
+                HttpClient.Redirect.NEVER,
+                new String[]{"TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"},
+                0
+        );
+        Execution execution = monitor.run();
+        assertEquals("SSL Connection problem: PKIX path validation failed: java.security.cert.CertPathValidatorException: validity check failed", execution.getErrorMessage());
+    }
+
+    @Test
+    void sslCertificate_expired_shouldNot_returnError() {
+        SslMonitor monitor = new SslMonitor(
+                "id",
+                "https://expired.badssl.com/",
+                1,
+                HttpClient.Version.HTTP_2,
+                HttpClient.Redirect.NEVER,
+                new String[]{"TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"},
+                0
+        );
+        assertDoesNotThrow(monitor::run);
+    }
+
+
 }
